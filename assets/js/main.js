e42332762ca4aa54260ef9d54d4bbb5ca0d41da1
Schlabauke <annabelle.jeffre@gmail.com>
2021-10-26 15:25:43 +0200

    Lev1_3JSInnerHTML

---
+++ assets/js/main.js
@@ -0,0 +1,4 @@
+//https://source.unsplash.com/random
+
+document.getElementById("gallery").innerHTML = "<figure style='display:inline-block'><img src='https://source.unsplash.com/300x300'><figcaption> Fig 1 </figcaption></figure><figure style='display:inline-block'> <img src='https://source.unsplash.com/300x300'><figcaption> Fig 2 </figcaption></figure><figure style='display:inline-block'><img src='https://source.unsplash.com/300x300'><figcaption> Fig 3 </figcaption></figure>"
+/* document.getElementById("gallery").style.textAlign = "center" */
